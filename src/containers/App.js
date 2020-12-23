a75f5e16e1e358b65eb9c0a39e542743ebb6065b
levani-creator <69543640+levani-creator@users.noreply.github.com>
2020-12-23 15:54:39 +0400

    Hello Robo

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state= {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users }))
+    
+  }
+
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value})
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    }
+    return(
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    )
+  }
+
+  
+}
+
+
+export default App
